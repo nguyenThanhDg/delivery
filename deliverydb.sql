976c1e3bfe3cf3f6827d962fa543833e2e313f9d
nguyenThanhDg <nguyenthanhdungktm@gmail.com>
2022-07-19 10:11:04 +0700

    lan 2

---
+++ deliverydb.sql
@@ -0,0 +1,245 @@
+-- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: deliverydb
+-- ------------------------------------------------------
+-- Server version	8.0.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `comment`
+--
+
+DROP TABLE IF EXISTS `comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `comment` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `customer` int NOT NULL,
+  `shipper` int NOT NULL,
+  `created_date` date NOT NULL,
+  `content` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `updated_date` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_comment_customer_idx` (`customer`),
+  KEY `fk_comment_shipper_idx` (`shipper`),
+  CONSTRAINT `fk_comment_customer` FOREIGN KEY (`customer`) REFERENCES `user_customer` (`id`),
+  CONSTRAINT `fk_comment_shipper` FOREIGN KEY (`shipper`) REFERENCES `user_shipper` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comment`
+--
+
+LOCK TABLES `comment` WRITE;
+/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `product` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `customer` int NOT NULL,
+  `name` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `price` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_date` date NOT NULL,
+  `active` bit(1) DEFAULT b'0',
+  `status` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_pro_cus_idx` (`customer`),
+  CONSTRAINT `fk_pro_cus` FOREIGN KEY (`customer`) REFERENCES `user_customer` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product`
+--
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product_shipper`
+--
+
+DROP TABLE IF EXISTS `product_shipper`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `product_shipper` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `shipper` int NOT NULL,
+  `product` int NOT NULL,
+  `cost` decimal(10,0) NOT NULL,
+  `created_date` date NOT NULL,
+  `updated_date` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_shipper_idx` (`shipper`),
+  KEY `fk_product_idx` (`product`),
+  CONSTRAINT `fk_product` FOREIGN KEY (`product`) REFERENCES `product` (`id`),
+  CONSTRAINT `fk_shipper` FOREIGN KEY (`shipper`) REFERENCES `user_shipper` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product_shipper`
+--
+
+LOCK TABLES `product_shipper` WRITE;
+/*!40000 ALTER TABLE `product_shipper` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product_shipper` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `rating`
+--
+
+DROP TABLE IF EXISTS `rating`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `rating` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `customer` int NOT NULL,
+  `shipper` int NOT NULL,
+  `rate` smallint NOT NULL,
+  `created_date` date NOT NULL,
+  `updated_date` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_rating_cus_idx` (`customer`),
+  KEY `fk_rating_shipper_idx` (`shipper`),
+  CONSTRAINT `fk_rating_cus` FOREIGN KEY (`customer`) REFERENCES `user_customer` (`id`),
+  CONSTRAINT `fk_rating_shipper` FOREIGN KEY (`shipper`) REFERENCES `user_shipper` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rating`
+--
+
+LOCK TABLES `rating` WRITE;
+/*!40000 ALTER TABLE `rating` DISABLE KEYS */;
+/*!40000 ALTER TABLE `rating` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_admin`
+--
+
+DROP TABLE IF EXISTS `user_admin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_admin` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `last_name` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `username` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `active` bit(1) DEFAULT b'1',
+  `avatar` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_date` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_admin`
+--
+
+LOCK TABLES `user_admin` WRITE;
+/*!40000 ALTER TABLE `user_admin` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_admin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_customer`
+--
+
+DROP TABLE IF EXISTS `user_customer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_customer` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `last_name` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `username` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `active` bit(1) DEFAULT b'1',
+  `created_date` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_customer`
+--
+
+LOCK TABLES `user_customer` WRITE;
+/*!40000 ALTER TABLE `user_customer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_customer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_shipper`
+--
+
+DROP TABLE IF EXISTS `user_shipper`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_shipper` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `last_name` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `username` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `active` bit(1) DEFAULT b'1',
+  `created_date` date NOT NULL,
+  `identity` int NOT NULL,
+  `avatar` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `address` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `dob` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_shipper`
+--
+
+LOCK TABLES `user_shipper` WRITE;
+/*!40000 ALTER TABLE `user_shipper` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_shipper` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-07-19 10:08:41
